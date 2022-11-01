1e41c632166abf674e1e4c2a12f63a11ebdef0a6
Max Lange <max@langes.us>
2022-11-01 18:47:49 -0400

    feat:Add save posters functionality

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+var saveButton = document.getElementById('save-button');
+var titleInput = document.getElementById('title-box');
+var bodyInput = document.getElementById('body-box');
+var ideaContainer = document.getElementById('bottom-container');
+
+var currentIdea;
+var savedIdeaList = [];
+
+
+saveButton.addEventListener('click', function(event){
+    event.preventDefault();
+    saveIdea(titleInput.value, bodyInput.value);
+})
+
+
+function saveIdea(title, body) {
+    if(titleInput.value && bodyInput.value){
+        currentIdea = new Idea(title, body);
+        ideaContainer.innerHTML += `<article class="saved-idea">
+        <header class="top-header">
+        <button class='starred-button'><img class="starred-button" src="assets/star-active.svg" /></button>
+        <button class='delete-button'><img class='delete-button' src='assets/delete.svg' /></button>
+        </header>
+        <h2>${currentIdea.title}</h2>
+        <p>${currentIdea.body}</p>
+        <footer class="footer-container">
+        <button class="comment-button"><img src="assets/comment.svg" /></button>
+        <label class="label-text">Comment</label>
+        </footer>
+        </article>`;
+        savedIdeaList.push(currentIdea);
+    }
+}
+
+
+
 {/* 
 <article class="saved-idea">
 <header class="top-header">
@@ -12,4 +48,3 @@
 </footer>
 </article>
  */}
-
