7e73bf1dfca5ece5eb0e01516d582f89d799b920
jordan smith <jordan.smith6035@gmail.com>
2022-11-01 16:20:11 -0600

    Feat/css-buildout

---
+++ main.js
@@ -0,0 +1,15 @@
+{/* 
+<article class="saved-idea">
+<header class="top-header">
+<button class='starred-button'><img class="starred-button" src="assets/star-active.svg" /></button>
+<button class='delete-button'><img class='delete-button' src='assets/delete.svg' /></button>
+</header>
+<h2>Idea title</h2>
+<p>This is what I was thinking. We just hang out all day and do nothing and just eat popcorn all freaking day and lay around. Its gonna be awesome!</p>
+<footer class="footer-container">
+<button class="comment-button"><img src="assets/comment.svg" /></button>
+<label class="label-text">Comment</label>
+</footer>
+</article>
+ */}
+
